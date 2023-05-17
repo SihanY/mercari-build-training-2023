a9f32cc443efb27e7ee9eb5fa9077a5bd49372d9
Sihan <SihanYang1@outlook.com>
2023-05-18 06:02:16 +0900

    STEP 3

--- go/app/main.go
+++ go/app/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -20,6 +23,16 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type Item struct {
+	Name          string `json:"name"`
+	Category      string `json:"category"`
+	ImageFilename string `json:"image_filename"`
+}
+
+type ItemList struct {
+	Items []Item `json:"items"`
+}
+
 func root(c echo.Context) error {
 	res := Response{Message: "Hello, world!"}
 	return c.JSON(http.StatusOK, res)
@@ -28,12 +41,69 @@ func root(c echo.Context) error {
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
-	c.Logger().Infof("Receive item: %s", name)
+	c.Logger().Infof("Receive item name: %s", name)
+	category := c.FormValue("category")
+	c.Logger().Infof("Receive item category: %s", category)
+	imageFileName := c.FormValue("image")
+	c.Logger().Infof("Receive item image: %s", imageFileName)
+
+	// sha256 handle image
+	h := sha256.New()
+	h.Write([]byte(imageFileName))
+	hashImageFileName := hex.EncodeToString(h.Sum(nil)) + ".jpg"
+
+	// Create item and add it to the end of items
+	item := Item{Name: name, Category: category, ImageFilename: hashImageFileName}
+	items, err := readItemsJson(c)
+	if err != nil {
+		return err
+	}
+	items = append(items, item)
+	itemList := ItemList{Items: items}
+
+	// Write data to items.json
+	jsonFile, err := os.Open("jsons/items.json")
+	if err != nil {
+		res := Response{Message: "error opening json file"}
+		return c.JSON(http.StatusBadRequest, res)
+	}
+	defer jsonFile.Close()
+	jsonData, err := json.MarshalIndent(itemList, "", "\t")
+	if err != nil {
+		res := Response{Message: "error converting json"}
+		return c.JSON(http.StatusBadRequest, res)
+	}
+	err = os.WriteFile("jsons/items.json", jsonData, 0777)
+	if err != nil {
+		res := Response{Message: "error writing json file"}
+		return c.JSON(http.StatusBadRequest, res)
+	}
 
 	message := fmt.Sprintf("item received: %s", name)
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
+}
+
+func getAllItems(c echo.Context) error {
+	items, err := readItemsJson(c)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, items)
+}
+
+func readItemsJson(c echo.Context) ([]Item, error) {
+	jsonData, err := os.ReadFile("jsons/items.json")
+	if err != nil {
+		res := Response{"error reading json file aa"}
+		return nil, c.JSON(http.StatusBadRequest, res)
+	}
+	var items ItemList
+	json.Unmarshal(jsonData, &items)
+
+	return items.Items, nil
 }
 
 func getImg(c echo.Context) error {
@@ -70,9 +140,9 @@ func main() {
 
 	// Routes
 	e.GET("/", root)
+	e.GET("/items", getAllItems)
 	e.POST("/items", addItem)
 	e.GET("/image/:imageFilename", getImg)
-
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
